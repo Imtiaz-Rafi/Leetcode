981d9b6da9c96e2271b31fd522a9bc5f5a7e2735
Imtiaz Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2022-07-05 22:45:41 +0600

    Time: 176 ms (46.84%), Space: 15 MB (22.23%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -3,24 +3,36 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         bool ok = false;
         int m = matrix.size();
-        for(int i=0;i<m;i++){
-            int n = matrix[i].size();
-            int l = 0;
-            int r =  n-1;
-            while(l<=r){
-                int mid = (l+r+1)/2;
-                if(matrix[i][mid]==target){
-                    ok = true;
-                    break;
-                }else if(matrix[i][mid]>target){
-                    r = mid-1;
-                }else{
-                    l = mid+1;
-                }
-            }
-            if(ok)
-                break;
+        int l = 0;
+        int r = matrix[0].size()-1;
+        while(l<matrix.size() && r>=0){
+            if(matrix[l][r]==target)
+                return true;
+            else if(matrix[l][r]>target)
+                r--;
+            else
+                l++;
         }
-        return ok;
+        return false;
+        // Limit = O(mlogn)
+        // for(int i=0;i<m;i++){
+        //     int n = matrix[i].size();
+        //     int l = 0;
+        //     int r =  n-1;
+        //     while(l<=r){
+        //         int mid = (l+r+1)/2;
+        //         if(matrix[i][mid]==target){
+        //             ok = true;
+        //             break;
+        //         }else if(matrix[i][mid]>target){
+        //             r = mid-1;
+        //         }else{
+        //             l = mid+1;
+        //         }
+        //     }
+        //     if(ok)
+        //         break;
+        // }
+        // return ok;
     }
 };
