75bcc92b94fa90f03e618e77afde2c5a87e43974
Imtiaz Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2022-10-17 16:48:51 +0600

    Time: 44 ms (58.18%), Space: 19.1 MB (97.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,19 +3,14 @@ public:
     void moveZeroes(vector<int>& nums) {
         int cnt = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                nums.erase(nums.begin()+i);
-                i--;
+            if(nums[i]!=0){
+                nums[cnt] = nums[i];
                 cnt++;
-                // nums.push_back(0);
-                // // cout<<nums[0]<<endl;
-                // for(int j=0;j<nums.size();j++){
-                //     cout<<nums[j]<<" ";
-                // }
-                // cout<<endl;
             }
         }
-        while(cnt>0)
-            nums.push_back(0),cnt--;
+        while(cnt<nums.size()){
+            nums[cnt] = 0;
+            cnt++;
+        }
     }
 };
