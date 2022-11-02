a83a6aac97b4129c8face25a5a6300a1cac6e3bc
Imtiaz Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2022-11-02 22:34:43 +0600

    Time: 1156 ms (14.46%), Space: 159.6 MB (13.54%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,8 +1,53 @@
 class Solution {
+    void merge(vector<int>& nums,int l,int m,int r){
+        // Create v ← nums[l..m] and v1 ← nums[m+1..r]
+        int x = m-l+1;
+        int y = r-m;
+        vector<int> v(x),v1(y);
+        for(int i=0;i<x;i++){
+            v[i] = nums[l+i];
+        }
+        for(int i=0;i<y;i++){
+            v1[i] = nums[m+1+i];
+        }
+        
+        // Maintain current Index of subarray and main Array
+        int i = 0,j = 0,k = l;
+        
+        while(i<x && j<y){
+            if(v[i]<=v1[j]){
+                nums[k] = v[i];
+                i++;
+            }else{
+                nums[k] = v1[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while(i<x){
+            nums[k] = v[i];
+            i++;
+            k++;
+        }
+        while(j<y){
+            nums[k] = v1[j];
+            j++;
+            k++;
+        }
+    }
+    void mergeSort(vector<int>& nums,int l,int r){
+        if(l<r){
+            int m = l+(r-l)/2;
+            mergeSort(nums,l,m);
+            mergeSort(nums,m+1,r);
+        
+            merge(nums,l,m,r);
+        }
+    }
 public:
     vector<int> sortArray(vector<int>& nums) {
-//         JUST FOR TESTING
-        sort(nums.begin(),nums.end());
+        mergeSort(nums,0,nums.size()-1);
         return nums;
     }
 };
