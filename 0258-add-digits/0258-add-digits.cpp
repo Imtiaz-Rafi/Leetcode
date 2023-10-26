71a2640a16c2ea6275addc6e9adf4c60d522d198
Imtiaj Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2023-10-26 19:34:05 +0600

    Time: 5 ms (15.79%), Space: 6.5 MB (10.52%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -2,19 +2,6 @@ class Solution {
 public:
     int addDigits(int num) {
         
-        int arr[9] = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
-        
-        while(num>=10){
-            int temp = num, ans = 0;
-            for(int i=8;i>=0;i--){
-                if(arr[i]<=temp){
-                    ans += temp%arr[i];
-                    temp = temp/arr[i]; 
-                }
-            }
-            num = ans+temp;
-        }
-        
-        return num;
+        return num%9==0 && num>0 ? 9 : num%9;
     }
 };
