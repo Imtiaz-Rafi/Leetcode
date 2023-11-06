b04b01b08e3a58dce175a765041184f9c18212f5
Imtiaj Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2023-11-06 16:10:09 +0600

    Time: 5 ms (73.12%), Space: 10.5 MB (66.51%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
+    static bool compare(int a, int b){
+        if(__builtin_popcount(a) == __builtin_popcount(b)){
+            return a<b;
+        }
+        return __builtin_popcount(a) < __builtin_popcount(b);
+    }
+    
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        vector<pair<int,int>> v;
-        vector<int>newArr;
-        
-        for(auto x:arr){
-            int bit = __builtin_popcount(x);
-            v.push_back(make_pair(x, bit));
-        }
-        
-        sort(v.begin(), v.end(), [&](const pair<int,int> &a,const pair<int,int> &b){return a.second==b.second? a.first<b.first : a.second<b.second;});
-        
-        for(auto x:v){
-            // cout<< x.first<< " "<< x.second<<endl;
-            newArr.push_back(x.first);
-        }
-            
-        
-        return newArr;
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
     }
 };
