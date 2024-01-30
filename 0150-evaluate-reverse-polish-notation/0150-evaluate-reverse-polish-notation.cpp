54c546f09cf2e161c1897a619bab8b1517cfce1a
Imtiaj Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2024-01-31 02:02:14 +0600

    Time: 12 ms (34.44%), Space: 15.4 MB (11.31%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -3,29 +3,24 @@ public:
     int evalRPN(vector<string>& tokens) {
         stack<int>s;
         
-        for(auto x:tokens){
-            if((x[0]>='0' && x[0]<='9') || (x.size()>1 && x[0]=='-' && (x[1]>='0' && x[1]<='9'))){
-                s.push(stoi(x));
-            }else{
-                int val1 = s.top();
+        for(auto t:tokens){
+            if(t == "+" || t == "-" || t == "/" || t == "*"){
+                int val1 = s.top(); 
                 s.pop();
                 int val2 = s.top();
                 s.pop();
                 
-                int ans;
-                if(x=="+"){
-                    ans = val2 + val1;
-                }else if(x=="-"){
-                    ans = val2 - val1;
-                }else if(x=="*"){
-                    ans = val2 * val1;
-                }else{
-                    ans = val2 / val1;
-                }
-                s.push(ans);
+                int res = 0;
+                if(t == "+") res = val2 + val1;
+                if(t == "-") res = val2 - val1;
+                if(t == "*") res = val2 * val1;
+                if(t == "/") res = val2 / val1;
+                
+                s.push(res);
+            }else{
+                s.push(stoi(t));
             }
         }
-        
         return s.top();
     }
 };
