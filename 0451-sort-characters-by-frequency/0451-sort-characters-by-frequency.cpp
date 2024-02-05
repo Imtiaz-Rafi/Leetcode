975fc02b80ced35cc6eab06f0e787ded201ff802
Imtiaj Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2024-02-06 01:55:08 +0600

    Time: 8 ms (82.45%), Space: 10.9 MB (13.99%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     string frequencySort(string s) {
         map<char, int>mp;
-        for(auto x:s) mp[x]++;
+        for(auto x:s) mp[x]++; // m[A] = 1, m[a] = 1, m[b] = 2
         
-        vector<pair<char, int>> v {mp.begin(), mp.end()};
+        vector<pair<char, int>> vec {mp.begin(), mp.end()}; // {A, 1}, {a,1}, {b,2}
         
-        sort(v.begin(), v.end(), [](pair<char, int> l, pair<char, int> r)
+        sort(vec.begin(), vec.end(), [](pair<char, int> l, pair<char, int> r)
         {
             return l.second > r.second;
         });
+        // {b, 2}, {A,1}, {a,1}
         
         string str;
-        for(auto x:v){
+        for(auto x:vec){
             while(x.second>0){
                 str+=x.first;
                 x.second--;
