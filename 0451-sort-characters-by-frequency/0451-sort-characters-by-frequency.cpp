58c2763bdb9aa7f8d30a4be03d8e9e3298d51b6f
Imtiaj Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2024-02-07 19:55:24 +0600

    Time: 12 ms (47.79%), Space: 10.9 MB (21.89%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        map<char, int>mp;
-        for(auto x:s) mp[x]++; // m[A] = 1, m[a] = 1, m[b] = 2
-        
-        vector<pair<char, int>> vec {mp.begin(), mp.end()}; // {A, 1}, {a,1}, {b,2}
-        
-        sort(vec.begin(), vec.end(), [](pair<char, int> l, pair<char, int> r)
-        {
+        map<char, int> mp;
+        for (auto x : s)
+            mp[x]++; // mp[A] = 1, mp[a] = 1, mp[b] = 2
+
+        vector<pair<char, int>> vec {mp.begin(), mp.end()}; //{A,1}, {a,1}, {b,2}
+
+        sort(vec.begin(), vec.end(), [](pair<char, int> l, pair<char, int> r) {
             return l.second > r.second;
         });
-        // {b, 2}, {A,1}, {a,1}
-        
+        // {b,2}, {A,1}, {a,1}
+
         string str;
-        for(auto x:vec){
-            while(x.second>0){
-                str+=x.first;
+        for (auto x : vec) {
+            while (x.second > 0) {
+                str += x.first;     //str = bbAa
                 x.second--;
             }
         }
         return str;
+
+        // Time Complexity = O(N)
+        // Space Complexity = O(N)
     }
 };
