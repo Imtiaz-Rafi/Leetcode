7eb7ec60b1d5e3b3befa39a725b6f14abeb6c041
Imtiaj Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2024-03-29 02:32:05 +0600

    Time: 199 ms (81.63%) | Memory: 148.4 MB (86.23%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -4,14 +4,13 @@ public:
         int i = 0,j = 0, res = 0; 
         unordered_map<int, int>m;
         while(i<nums.size()){
-            if(m[nums[i]]+1 > k && i>=j){
+            m[nums[i]]++;
+            while(m[nums[i]] > k && i>=j) {
                 m[nums[j]]--;
                 j++;
-            }else{
-                res = max(res, (i-j+1));
-                m[nums[i]]++;
-                i++;
-            }
+            };
+            res = max(res, (i-j+1));
+            i++;
         }
         return res;
     }
