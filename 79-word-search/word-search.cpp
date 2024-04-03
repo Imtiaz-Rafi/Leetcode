7f5f00848e2b397d62553f1e770c3474b8794045
Imtiaj Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2024-04-04 01:10:36 +0600

    Time: 1027 ms (22.04%) | Memory: 11.7 MB (34.90%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -12,16 +12,16 @@ public:
         char temp = board[i][j];
         board[i][j] = '#';
 
-        bool ans = dfs(i+1, j, count+1, board, word) ||
-                dfs(i-1, j, count+1, board, word) ||
-                dfs(i, j+1, count+1, board, word) ||
-                dfs(i, j-1, count+1, board, word);
+        bool ans =  dfs(i+1, j, count+1, board, word) ||
+                    dfs(i-1, j, count+1, board, word) ||
+                    dfs(i, j+1, count+1, board, word) ||
+                    dfs(i, j-1, count+1, board, word);
+        
         board[i][j] = temp;
         return ans;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<int>>a;
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[i].size();j++){
                 if(board[i][j]==word[0] && dfs(i,j,0,board, word)){
