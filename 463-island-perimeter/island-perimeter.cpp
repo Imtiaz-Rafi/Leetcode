176d886d72dd29bf2fc1a6a9743862e2bcdd1780
Imtiaj Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2024-04-19 01:06:01 +0600

    Time: 83 ms (31.44%) | Memory: 100.7 MB (94.99%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -7,30 +7,14 @@ public:
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==1){
-                    if(i==0){
-                        ans++;
-                    }
-                    if(i==row-1){
-                        ans++;
-                    }
-                    if(j==0){
-                        ans++;
-                    }
-                    if(j==col-1){
-                        ans++;
-                    }
-                    if(i>0 && grid[i-1][j]==0){
-                        ans++;
-                    }
-                    if(i<row-1 && grid[i+1][j]==0){
-                        ans++;
-                    }
-                    if(j>0 && grid[i][j-1]==0){
-                        ans++;
-                    }
-                    if(j<col-1 && grid[i][j+1]==0){
-                        ans++;
-                    }
+                   ans += 4;
+
+                   if(i>0 && grid[i-1][j]==1){
+                    ans -= 2;
+                   }
+                   if(j>0 && grid[i][j-1]==1){
+                    ans -= 2;
+                   }
                 }
             }
         }
