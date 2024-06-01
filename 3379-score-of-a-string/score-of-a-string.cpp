c1aaac5b28e27939680d28f0d0991376f441d43b
Imtiaj Rafi <66630714+Imtiaz-Rafi@users.noreply.github.com>
2024-06-01 20:44:12 +0600

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.38%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int scoreOfString(string s) {
         int ans = 0;
         for(int i=0;i<s.size()-1;i++){
-            ans += abs((s[i] - '0') - (s[i+1] - '0'));
+            ans += abs(s[i] - s[i+1]);
         }
         return ans;
     }
